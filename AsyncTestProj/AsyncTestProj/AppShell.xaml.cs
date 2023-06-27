c590c45b7fd2354210caf1b80aaad284735c22d7
Luke Baldis <75636495+LukeTaylorB@users.noreply.github.com>
2023-06-27 02:08:36 -0400

    test

--- AsyncTestProj/AsyncTestProj/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AsyncTestProj;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
--- LegalProjects/LegalProjects/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LegalProjects;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
